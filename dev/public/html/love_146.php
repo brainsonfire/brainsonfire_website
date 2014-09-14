8e503b09e5c3f832b91e56dccdbfb4cf41b95015
Dejan Stankovic <dejan@skippaz.com>
2014-09-15 01:41:10 +0200

    love 146 page HTML

--- dev/public/html/love_146.php
+++ dev/public/html/love_146.php
@@ -18,11 +18,246 @@
 
 
 			
-			<main id="main">
+			<main id="main" class="case-study">
 
 
+
+				<div class="direction-nav top">
+
+					<div class="row">
+
+						<a class="previous" href="#dummy">
+							<span id="nav-text">previous story</span>
+						</a>
+
+						<a class="previous" href="#dummy">
+							<span id="nav-text">back to all case studies</span>
+						</a>
+
+						<a class="next" href="#dummy">
+							<span id="nav-text">next story</span>
+						</a>
+
+					</div>
+
+				</div>
+				
+
+
+				<div id="hero" class="main-photo">
+
+					<div class="row">
+
+						<img src="images/case-study/love-146/love146.png" alt="">
+						
+					</div>
+				
+				</div>
+
+
+
+				<section id="love-146-intro" class="intro">
+
+					<div class="row">
+
+						<h1>Love 146</h1>
+
+						<p>In 2007, Justice for Children International was a small organization with a noble goal: the abolition of child sex trafficking and exploitation through prevention and aftercare. Unfortunately, a Texas-based not-for-profit organization with a different focus was operating under the same name. The confusion and overlap led to a necessary name change.</p>
+
+						<p>Ultimately, what began as a simple identity shift would end up revolutionizing and re-energizing the organization, sparking a global movement and a worldwide network of abolitionists working to end child sex trafficking. “When we left Brains on Fire [after our initial meeting], we got into our car, looked at each other, smiled and said, ‘same tribe.’ – Rob Morris, Love 146, Executive Director</p>
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="video-section">
+
+					<div class="row">
+
+						<h3>In Rob’s Own Words</h3>
+
+						<div class="videoHolder">
+							
+							 <iframe width="560" height="315" src="//www.youtube.com/embed/XPxmWBT-lPk" frameborder="0" allowfullscreen></iframe>
+
+						</div>
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="featured red">
+
+					<div class="row">
+
+						<h3>The purpose</h3>
+
+						<ul>
+							<li>– Create a powerful, memorable name that would start a conversation.</li>
+							<li>– Ignite a movement to wipe child sex slavery off the face of the planet.</li>
+							<li>– Raise capital from public and private sectors.</li>
+						</ul>
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="image-section">
+
+					<div class="row">
+
+						<h3>Primary Name & Mark</h3>
+
+						<img src="images/case-study/love-146/primary_name.jpg" alt="">
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="image-section two-col">
+
+					<div class="row">
+						
+						<h3>Stationary & Book Concept</h3>
+
+						<img src="images/case-study/love-146/book_concept_01.jpg" alt="">
+
+						<img src="images/case-study/love-146/book_concept_02.jpg" alt="">
+
+						<img src="images/case-study/love-146/book_concept_03.jpg" alt="">
+
+						<img src="images/case-study/love-146/book_concept_04.jpg" alt="">
+
+					</div>
+				
+				</section>
+
+
+
+
+
+
+				<section class="featured gray20">
+
+					<div class="row">
+
+						<h3>The 146 Patch</h3>
+
+						<p>Though the identity of the young girl with the tattered “146” tag pinned to her dress remains unknown, thousands of Love146 supporters stand in solidarity by wearing the very same number today. This simple patch has become a powerful symbol of hope and action proudly adorned by those who have joined the fight to end child sex trafficking.</p>
+
+						<img src="images/case-study/love-146/the146patch.jpg" alt="">
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="featured gray20">
+
+					<div class="row">
+
+						<h3>Personal Expression</h3>
+
+						<p>From Love146-branded shoes to 146 tag tattoos, Love146 advocates and supporters continue finding unique ways to carry the message and mission of the organization out into the world in their own way.</p>
+
+						<img src="images/case-study/love-146/expression.png" alt="">
+						
+					</div>
+				
+				</section>
+
+
+
+				<section class="video-section">
+
+					<div class="row">
+
+						<h3>In Rob’s Own Words</h3>
+
+						<div class="videoHolder">
+							
+							 <iframe width="560" height="315" src="//www.youtube.com/embed/XPxmWBT-lPk" frameborder="0" allowfullscreen></iframe>
+
+						</div>
+						
+					</div>
+				
+				</section>
+
+
+
+				<section id="love-146-success">
+
+					<div class="row">
+
+						<h3>Success</h3>
+						
+						<ul>
+							<li>– Quote from the Executive Director, Rob Morris: “We are daily discovering the genius behind the Love146 name and ‘brand’ you unearthed for us. I remember you addressing our initial concerns of the obscurity of a name like Love146. You said it will create intrigue and force the story... ‘her story’ to be told. Well... it’s happening.”</li>
+							<li>– A Church in Long Island, New York was just given a winning 3 million dollar lottery ticket by a member of their congregation. The Church is donating $200,000 of it to Love146. This made big news, so the Pastor has been on Good Morning America and The Early Show talking about Love146. Great awareness!</li>
+							<li>– Baume and Mercier, the luxury watch company, has become a corporate sponsor and is creating a photo exhibit. They hired a Pulitzer prize winning photographer to travel to southeast Asia to create the project inspired by work from Love146, which will feature stories of hope and restoration.</li>
+							<li>– The band Paramore has adopted the cause by making their audience aware of Love146 on their tours and encouraging donations. They wear the Love146 patch on their clothes and instruments in concert, like when they played the MTV Music Awards.</li>
+							<li>– The owner of a Porsche racing team is giving Love146 the opportunity to design a Love146 Porsche race car to wrap for the upcoming Lemans in California as well as the 24 hour Daytona race in January. This will be broadcast on NBC.</li>
+						</ul>
+						
+					</div>
+				
+				</section>
+
+
+
+				<div id="love-146-bottom">
+
+					<div class="row">
+
+						<img src="images/case-study/love-146/love146org.jpg" alt="">
+
+					</div>
+				
+				</div>
+
+
+				<div class="books-bottom">
+
+					<div class="row">
+
+						<a href="#dummy" class="button btn-blue">learn how we can help you ignite a movement</a>
+					
+					</div>
+				
+				</div>
 
 				
+
+
+				<div class="direction-nav bottom">
+
+					<div class="row">
+
+						<a class="previous" href="#dummy">
+							<span id="nav-text">previous story</span>
+						</a>
+
+						<a class="previous" href="#dummy">
+							<span id="nav-text">back to all case studies</span>
+						</a>
+
+						<a class="next" href="#dummy">
+							<span id="nav-text">next story</span>
+						</a>
+
+					</div>
+					
+				</div>
 
 
 
