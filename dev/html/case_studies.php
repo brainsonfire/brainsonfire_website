a1415d64dd55ab73433facbfe754e2fe26ebc0ee
Dejan Stankovic <dejan@skippaz.com>
2014-09-18 11:16:57 +0200

    Case studies page code polishing, image optimization

--- dev/html/case_studies.php
+++ dev/html/case_studies.php
@@ -4,7 +4,7 @@
 	<!--[if lte IE 8]>
 		<p class="chromeframe"> <strong>You are using an outdated browser.</strong><br /> <br />Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	<![endif]-->
-	
+
 	<div class="off-canvas-wrap" data-offcanvas>
 		<div class="inner-wrap">
 			
@@ -18,15 +18,83 @@
 						<img src="images/case-study/case_studies_title.png" alt="">
 					</div>
 				</section>
+
+				<div id="projects-list">
 				
-				<ul class="row project-list">
-					<li><a href="#dummy"><img src="images/case-study/project_01.jpg" alt=""></a></li>
-					<li><a href="#dummy"><img src="images/case-study/project_02.jpg" alt=""></a></li>
-					<li><a href="#dummy"><img src="images/case-study/project_03.jpg" alt=""></a></li>
-					<li><a href="#dummy"><img src="images/case-study/project_04.jpg" alt=""></a></li>
-					<li><a href="#dummy"><img src="images/case-study/project_05.jpg" alt=""></a></li>
-					<li><a href="#dummy"><img src="images/case-study/project_06.jpg" alt=""></a></li>
-				</ul>
+					<div class="row">
+
+						<ul class="row project-list">
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_01.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_02.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_03.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_04.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_05.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+							<li class="large-4 medium-4 small-12 columns">
+								<a href="#dummy">
+									<img src="images/case-study/project_06.jpg" alt="">
+									<span class="rollover">
+										<span class="middle">
+											<span class="title">Lorem Ipsum</span>
+											<span class="desc">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</span>
+										</span>
+									</span>
+								</a>
+							</li>
+						</ul>
+
+					</div>
+
+				</div>
 			
 			</main>
 			
