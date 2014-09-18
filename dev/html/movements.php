cc2ebef01e861b62d1673f648d7edd342e2b12b9
Dejan Stankovic <dejan@skippaz.com>
2014-09-18 09:44:43 +0200

    code polishing, home & movements pages

--- dev/html/movements.php
+++ dev/html/movements.php
@@ -4,7 +4,7 @@
 	<!--[if lte IE 8]>
 		<p class="chromeframe"> <strong>You are using an outdated browser.</strong><br /> <br />Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	<![endif]-->
-	
+
 	<div class="off-canvas-wrap" data-offcanvas>
 		<div class="inner-wrap">
 			
@@ -23,12 +23,12 @@
 					<div class="row">
 						<h1>What is a movement?</h1>
 						<div class="text-orbit-nav">
-							<a data-orbit-link="headline-1">What is a Movement?</a>
-							<a data-orbit-link="headline-2">Why Should You Care?</a>
-							<a data-orbit-link="headline-3">What Does It Take?</a>
-							<a data-orbit-link="headline-4">What Will It Do?</a>
+							<a href="#" data-orbit-link="headline-1">What is a Movement?</a>
+							<a href="#" data-orbit-link="headline-2">Why Should You Care?</a>
+							<a href="#" data-orbit-link="headline-3">What Does It Take?</a>
+							<a href="#" data-orbit-link="headline-4">What Will It Do?</a>
 						</div>
-						<ul class="text-orbit" data-orbit data-options="bullets: false; slide_number: false; timer: false; circular: false;">
+						<ul class="text-orbit" data-orbit data-options="bullets: false; slide_number: false; timer: false;">
 							<li data-orbit-slide="headline-1"><div>Today the word “movement” is thrown around by marketers to mean anything that can be measured in “likes” and “followers” and “shares”. But we stand by the original definition of the word. We know movements are not ignited by “likes”, they are ignited by people. Real people with real names. People interested in more than just winning a contest or promotion. We’re talking about passionate people who are connected and willing to put in a little elbow grease for something they believe in.</div></li>
 							<li data-orbit-slide="headline-2"><div><strong>So why should you CARE?</strong> Because for your company a movement can serve as a cultural compass and gravitational force that pulls people together. We believe you are in the business of more than just business and right now, as you read this, thousands of people are waiting to link arms with your brand and do something meaningful.</div></li>
 							<li data-orbit-slide="headline-3"><div><strong>What Does it Take?</strong> Our movements come with prerequisites. You must be passionate about your brand. You must have a strong commitment to outstanding customer service and a true love for your customers and employees. And most importantly, you must have a willingness to partner with people, and be brave enough to “be famous for the people who love you.”</div></li>
@@ -46,10 +46,10 @@
 						<h1>Here's How We Work</h1>
 						<p>Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis.</p>
 						<div class="circle-orbit-nav movement-orbit">
-							<a data-orbit-link="circle-headline-1"><span>Soul Mining /<br> Participation /<br> Getting Our Hands Dirty <span>60 days</span></span></a>
-							<a data-orbit-link="circle-headline-2"><span>Reframing<br> the Passion<br> Conversation <span>60 days</span></span></a>
-							<a data-orbit-link="circle-headline-3"><span>Word of Mouth Plan <span>60 days</span></span></a>
-							<a data-orbit-link="circle-headline-4"><span>Implementation <span>time/cost</span></span></a>
+							<a href="#" data-orbit-link="circle-headline-1"><span>Soul Mining /<br> Participation /<br> Getting Our Hands Dirty <span>60 days</span></span></a>
+							<a href="#" data-orbit-link="circle-headline-2"><span>Reframing<br> the Passion<br> Conversation <span>60 days</span></span></a>
+							<a href="#" data-orbit-link="circle-headline-3"><span>Word of Mouth Plan <span>60 days</span></span></a>
+							<a href="#" data-orbit-link="circle-headline-4"><span>Implementation <span>time/cost</span></span></a>
 						</div>
 						<ul class="circle-orbit" data-orbit data-options="bullets: false; slide_number: false; timer: false; navigation_arrows: true;">
 							<li data-orbit-slide="circle-headline-1"><div><span class="small-title">Soul Mining /<br> Participation /<br> Getting Our Hands Dirty <span>60 days</span></span><strong>If you’re looking for answers that no one else has, ask questions that no one else asks.</strong> During the insight phase we gather knowledge by listening to, learning about and exploring your company, offerings, market, competitors and customers. Brains on Fire staff will visit your offices to guide you through a series of interviews and exercises designed to help us become students of your business. We’ll have deep conversations with your biggest fans to learn why they love you so much and how your brand or organization fits into their lives. We’ll also explore brands, communities and experts beyond your immediate neighborhood.</div></li>
