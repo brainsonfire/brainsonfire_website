2d8d39211ff2b7df75066aa69d4b560229fa9288
Nathan Spainhour <nathans@nate-spain-yo.innovate.com>
2014-11-20 16:58:44 -0500

    New Heroes in Recovery Case Study

--- heroes_in_recovery-Backup.php
+++ heroes_in_recovery-Backup.php
@@ -36,7 +36,7 @@
 			
 			<?php include("inc/header.php"); ?>
 			
-			<main id="work-main" class="case-study inner-pages">
+			<main id="main" class="case-study inner-pages">
 				
 				<div class="direction-nav top">
 					<div class="row">
@@ -54,31 +54,41 @@
 
 				<section id="heroes-intro" class="intro">
 					<div class="row">
-							<h1>Heroes in Recovery</h1>
-						<div class="text bg-white"> 
-							<h3>INJUSTICE</h3>
-							<p>23 million people struggle with addiction in America. It is estimated 20 million of them do not seek treatment or help. </p>
-							<h3>Meet the Foundations Recovery Network. </h3>
-							<p>FRN is a leader in the treatment of co-occurring mental illness and substance abuse disorders. FRN needed a way to break through the noise and connect with the millions of people who were not seeking the help they needed, including the families and friends of those suffering from the disease.</p> 
-
-							<h3>REVEAL THE GOOD. (The good part)</h3>
-							<p>As we dug in, we began to see that people weren’t tuning in for more conversations about the addiction – they wanted to talk about the awesome power in recovery. No one walks path of recovery alone. From doctors to staff, families to friends to those suffering with addiction themselves, the journey takes a village – and each story is worthy of a celebration.<br />
- 
-Traditional programs shrouded the recovery journey in shame and anonymity. FRN recognized that in order to reach those who needed them most, that had to change.</p>  
-
-							<h3>Opportunity</h3>
-							<ul>
-								<li>Support people who want to help break the stigma of addiction. </li>
-								<li>To give people who want to talk about breaking the stigma to share. </li>
-							</ul>
-						</div>		
+						<h1>Heroes in Recovery</h1>
+						<div class="two-col">
+							<div class="left">
+								<p>The Foundations Recovery Network approached BOF with the number 23 million. That is the number of people that suffer from substance addiction. When you compare that to 25 million people suffering from cancer and 33 million people suffering from HIV, that number gets put into perspective.</p>
+							</div>
+							<div class="right">
+								<p>Due to the social stigma associated with substance addiction, only 2.6 million people are seeking treatment. That’s where Heroes in Recovery comes in – a movement that’s sole purpose is to reach the other 20 million. Substance addiction is a disease. Break the stigma, once and for all.</p>
+							</div>
+						</div>
 					</div>					
+				</section>
+
+				<section class="image-section no-padding">
+					<div class="row">
+						<h3>Lee Pepper, Foundations Recovery Network</h3>
+						<div class="block orange">
+							<img src="images/case-study/heroes/lee_pepper.jpg" alt="Lee Pepper">
+							<p>“A lot of the social ills we have stem from shame. People forget how far breast cancer awareness—and cancer awareness in general—has come. I heard an interview with comedian Bernie Mac years ago on Howard Stern. He was talking about how when he was a child in the 70s, his mother went away for six weeks—and he was just told she was ‘away.’ She was actually in the hospital having a double mastectomy. But you didn’t talk about that back then. Getting breast cancer was a shameful thing—like she had done something wrong.” “This is 2013!” Why are we still repeating the same mistakes that we learned from these other social movements? We have got to take action now. We must be able to talk about addiction and the mental illness that often comes with it openly and without shame, -- or else we’re still going to have not only thousands of people each year dying from this disease, but also impacting millions of lives needlessly... in a negative way.”</p>
+						</div>
+					</div>
 				</section>
 
 				<section class="image-section">
 					<div class="row">
 						<h3>Movement Name & Mark</h3>
 						<img src="images/case-study/heroes/movement_name.jpg" alt="">
+					</div>
+				</section>
+
+				<section class="featured">
+					<div class="row">
+						<div class="text bg-lemon">
+							<h3>The purpose</h3>
+							<p>Make recovery – not addiction – the newsworthy story and topic of conversation. Break the stigma associated with addiction by celebrating everyday heroes. Create a new voice of hope.</p>
+						</div>
 					</div>
 				</section>
 
@@ -103,9 +113,12 @@ Traditional programs shrouded the recovery journey in shame and anonymity. FRN r
 					</div>
 				</section>
 
-				<section class="image-section">
+				<section class="featured">
 					<div class="row">
-						<h3>The Journey Box</h3>
+						<div class="text bg-neptune">							
+							<h3>The Journey Box</h3>
+							<p>When someone arrives at an FRN treatment center, they are now welcomed into the program with a Journey Box. The Journey Box is a keepsake that FRN clients are encouraged to fill with anything and everything that tells their story: photos, good luck charms, letters, rocks, keys, reminders of good days and bad days—whatever helps them tell the world about their recovery journey. This conversation tool is used internally with FRN staff and patients to inspire, identify and celebrate stories they could share, not only in person, but also online at www.HeroesInRecovery.com.</p>
+						</div>
 						<img src="images/case-study/heroes/journey_box.jpg" alt="">
 						<ul class="gallery">
 							<li><img alt="" src="images/case-study/heroes/you_can_be_a_hero.jpg"></li>
@@ -115,9 +128,12 @@ Traditional programs shrouded the recovery journey in shame and anonymity. FRN r
 					</div>
 				</section>
 
-				<section class="image-section">
+				<section class="featured">
 					<div class="row">
-						<h3>Heroes 6K</h3>
+						<div class="text bg-orange">
+							<h3>Heroes 6K</h3>
+							<p>HEROES in RECOVERY is holding events across the country, seeking to inspire a sense of community wherever they can. HEROES signature event is the Heroes 6K Run and Walk – not 5K, but 6K – to create awareness of the need for treatment and to support those who are in recovery. The number six is meaningful, not only because it represents the six heroic lives woven into the word HEROES; it also underscores the significance of pushing yourself just a little bit harder and propelling yourself just a little bit further down the road to recovery. The inaugural 6K was held in 2011 on a race course in Leiper’s Fork, Tenn., during the month of September (National Recovery Month). It was sold out with 250 participants and around 100 volunteers. Since then, we have had a number of successful races across the country; in Palm Springs, Roswell Georgia, Greenville South Carolina, Coconut Creek, Florida, Louisville, Colorado and Memphis Tennessee. The amount of races — and participants — continues to grow!</p>
+						</div>
 						<img src="images/case-study/heroes/heroes_6k.jpg" alt="">
 						<ul class="gallery">
 							<li><img alt="" src="images/case-study/heroes/heroes_6k_01.jpg"></li>
@@ -136,9 +152,12 @@ Traditional programs shrouded the recovery journey in shame and anonymity. FRN r
 					</div>					
 				</section>
 
-				<section class="image-section">
+				<section class="featured">
 					<div class="row">
-						<h3>Share it with Sean</h3>
+						<div class="text bg-wedgewood">
+							<h3>Share it with Sean</h3>
+							<p>Conventional wisdom has taught us that it is the journey – not the destination – that matters most. Although we may walk different paths, the journey serves as our greatest teacher, our daily learning opportunity and ultimately, the story of our lives. At Brains on Fire everything we do is rooted in the belief that it’s about the people. By lifting people up and giving them a place to share their stories, BOF helped FRN lay the foundation for an important conversation that continues to reframe the way the world thinks and talks about addiction. As a result, FRN and the Heroes in Recovery Community have become the faces and voices of an ongoing celebration of life in recovery that is opening hearts, breaking stigmas and changing lives.</p>
+						</div>
 						<img src="images/case-study/heroes/share_it.jpg" alt="">
 						<img class="left top" src="images/case-study/heroes/badge_01.jpg" alt="">
 						<img class="right" src="images/case-study/heroes/road_trip.jpg" alt="">
