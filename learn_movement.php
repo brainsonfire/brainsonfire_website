8913d013833225ad0304d7f6b55474921077e930
Nathan Spainhour <nathans@nate-spain-yo-3.innovate.com>
2014-12-15 15:41:57 -0500

    rearranged circles

--- learn_movement.php
+++ learn_movement.php
@@ -44,27 +44,8 @@
 						<p>It all starts here</p>
 					</div>
 				</section>
-				
-				<section id="help">
-					<div class="row">
-						<h1>We can help you find your way</h1>
-						<p>Go ahead... tap our brains</p>
-						<div class="half-block">
-							<a href="2x2_workshops.php"><img src="images/movement/learn-1.jpg" alt="2x2 Workshop"></a>
-							<h2>2x2 Workshop</h2>
-							<p>We understand that travel can pose a challenge at times, which is precisely why we created the 2x2 Workshop. Instead of asking you to bring your team to Brains on Fire, we’ll send two of our strategic partners to you. Don’t worry, the dogs aren’t required... unless, of course, you’d like them to be.</p>
-							<a href="2x2_workshops.php" class="button btn-green">learn more</a>
-						</div>
-						<div class="half-block">
-							<a href="gut_check.php"><img src="images/movement/learn-2.jpg" alt="Get check my thinking"></a>
-							<h2>Gut check my thinking</h2>
-							<p>Feeling uncertain about the marketing programs your company is implementing to build its brand and increase sales? Want to bounce an idea off the brains of proven marketing pros? Sounds like you could use a Gut Check My Thinking chat with Brains on Fire.</p>
-							<a href="gut_check.php" class="button btn-green">learn more</a>
-						</div>
-					</div>
-				</section>
 
-				<section id="other-way" class="white-text">
+				<section id="help">
 					<div class="row">
 						<h1>Other ways we can help</h1>
 						<p>Get your learn on</p>
@@ -79,6 +60,25 @@
 							<h2>Speaking</h2>
 							<p>We’ve presented at conferences (big and small) spreading passion, inspiration, and oodles of marketing know-how around the globe. Interested in bringing one of our speakers to your event? Inquire within.</p>
 							<a href="speaking.php" class="button btn-orange">learn more</a>
+						</div>
+					</div>
+				</section>
+				
+				<section id="other-way" class="white-text">
+					<div class="row">
+						<h1>We can help you find your way</h1>
+						<p>Go ahead... tap our brains</p>
+						<div class="half-block">
+							<a href="2x2_workshops.php"><img src="images/movement/learn-1.jpg" alt="2x2 Workshop"></a>
+							<h2>2x2 Workshop</h2>
+							<p>We understand that travel can pose a challenge at times, which is precisely why we created the 2x2 Workshop. Instead of asking you to bring your team to Brains on Fire, we’ll send two of our strategic partners to you. Don’t worry, the dogs aren’t required... unless, of course, you’d like them to be.</p>
+							<a href="2x2_workshops.php" class="button btn-green">learn more</a>
+						</div>
+						<div class="half-block">
+							<a href="gut_check.php"><img src="images/movement/learn-2.jpg" alt="Get check my thinking"></a>
+							<h2>Gut check my thinking</h2>
+							<p>Feeling uncertain about the marketing programs your company is implementing to build its brand and increase sales? Want to bounce an idea off the brains of proven marketing pros? Sounds like you could use a Gut Check My Thinking chat with Brains on Fire.</p>
+							<a href="gut_check.php" class="button btn-green">learn more</a>
 						</div>
 					</div>
 				</section>
